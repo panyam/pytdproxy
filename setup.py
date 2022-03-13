e38400c030c9b8c7c08bf46ab1736db3b3155e76
Sri Panyam <sri.panyam@gmail.com>
2022-03-12 23:09:37 -0800

    Trying fixes for setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
     name="pytdproxy",
-    version="0.0.2",
-    author="Sri Panyam",
-    author_email="sripanyam@gmail.com",
-    description="Python GRPC Bindings for tdproxy - A local proxy for TD Ameritrade API (including streaming API)",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/panyam/pytdproxy",
-    project_urls={
-        "Bug Tracker": "https://github.com/panyam/pytdproxy/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
 )
